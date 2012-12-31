3f29090651e2e2a049d4d28d3a3d6347e4ff1ad1
douglas-vaz <douglas1992@gmail.com>
2012-12-31 21:36:27 +0530

    Correct Python version in flask virtualenv. Add app structure

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/douglas/workspace/flask"
+set -gx VIRTUAL_ENV "/home/douglas/workspace/Bubbles/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
